1ada7b14a255472b8682b0a632dbd58e5492dd06
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-25 16:18:18 -0300

    anotações das estruturas do projeto

--- estrutura_do_projeto/Program.cs
+++ estrutura_do_projeto/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// program.cs é o nome da classe onde codifica 
+// csproj geralmente nao precisa alterar
+// pasta obj nao precisa mexer, sao arquivos de compilação 
+// pasta bin é pasta de binarios
+// nao precisamos se preocupar com as pastas
