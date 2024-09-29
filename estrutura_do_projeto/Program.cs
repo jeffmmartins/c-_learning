f7ad30add3269cb18f589115e1edca1a0cc51dc2
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-29 16:09:39 -0300

    exemplo de variaveis

--- estrutura_do_projeto/Program.cs
+++ estrutura_do_projeto/Program.cs
@@ -1,7 +1,14 @@
 ﻿using estrutura_do_projeto.Models;
 
-Pessoa p = new Pessoa();
+string apresentação = "olá seja bem vindo"; // variavel
+
+int idade = 36;
+
+double altura = 1.90;
+
+
+/*Pessoa p = new Pessoa();
 
 p.Nome = "Jeff";
 p.Idade = 36;
-p.Apresentar();
+p.Apresentar();*/
