11f9c9a7979e5b1eb9c32ffcf40308e77ccd1096
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-10-17 20:58:52 -0300

    adicionando referencias

---
+++ FundamentosExemplos/exemplofundamentos/Program.cs
@@ -0,0 +1,37 @@
+﻿string opcao;
+string nome;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.WriteLine("1: Cadastrar Cliente");
+    Console.WriteLine("2: Buscar Cliente");
+    Console.WriteLine("3: Apagar registro");
+    Console.WriteLine("4: Encerrar programa");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+        Console.WriteLine("Digite o nome do cliente");
+        nome = Console.ReadLine();
+        Console.WriteLine($"{nome} Cadastrado com Sucesso");
+        break;
+        case "2":
+        Console.WriteLine("Busca de Cliente");
+        break;
+        case "3":
+        Console.WriteLine("Registro apagado");
+        break;
+        case "4":
+        Console.WriteLine("Encerrar programa");
+        exibirMenu = false;
+        break;
+        default:
+        Console.WriteLine("opção invalida");
+        break;
+        
+    }
+}
+
--- FundamentosExemplos/exemplofundamentos/obj/Debug/net8.0/exemplofundamentos.AssemblyInfo.cs
+++ FundamentosExemplos/exemplofundamentos/obj/Debug/net8.0/exemplofundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemplofundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a490bfce0344dbe216012bb6a18dc548c283ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05bc8b5adaa3c635ecac536793b043da71ccfe14")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemplofundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemplofundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
