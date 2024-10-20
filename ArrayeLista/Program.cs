2b9e68db0c30cac5c8823b40168d8ccc49137456
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-10-20 17:39:42 -0300

    acessando o array com o for

--- ArrayeLista/Program.cs
+++ ArrayeLista/Program.cs
@@ -4,6 +4,8 @@ arrayDeInteiros[0] = 1;
 arrayDeInteiros[1] = 2;
 arrayDeInteiros[2] = 3;
 
-Console.WriteLine(arrayDeInteiros);
+for(int contador = 0; contador < arrayDeInteiros.Length; contador++){
+    Console.WriteLine($"Posição {contador}: {arrayDeInteiros[contador]}");
+}
 
 
